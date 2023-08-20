640e1cca3dd118cae4aa544f971f823751cc3f64
unknown <sahu.himanshu2016@gmail.com>
2023-08-20 20:07:15 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import Movieapp from './Components/useeffect-02/Movieapp';
 import Foodrecipe from './Components/React-useeffect-001/Foodrecipe';
+// import Movieapp from './Components/useeffect-02/Movieapp';
+Foodrecipe
 
 function App() {
   return (
     <div className="App">
+      {/* <Movieapp /> */}
+
        {/* <Movieapp /> */}
        <Foodrecipe />
     </div>
